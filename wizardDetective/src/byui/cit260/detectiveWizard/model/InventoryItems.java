69ee1c47054273eb323c7b7907942293bf5ae7fa
lauryn <lauryn@10.40.16.39>
2015-01-30 23:50:54 -0700

    Inventory Items Comments

---
+++ wizardDetective/src/byui/cit260/detectiveWizard/model/InventoryItems.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.detectiveWizard.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+/**
+ *
+ * @author lauryn
+ */
+public class InventoryItems implements Serializable{
+    
+    //class instance variables
+    private String description;
+    private String itemName;
+
+    public InventoryItems() {
+    }
+
+    @Override
+    public String toString() {
+        return "InventoryItems{" + "description=" + description + ", itemName=" + itemName + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 31 * hash + Objects.hashCode(this.description);
+        hash = 31 * hash + Objects.hashCode(this.itemName);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final InventoryItems other = (InventoryItems) obj;
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.itemName, other.itemName)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+    
+    
+}
