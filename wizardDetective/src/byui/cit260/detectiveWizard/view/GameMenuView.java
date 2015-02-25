62d06f139de9cf449add46b3bb74055aaf863c8b
lauryn <lauryn@10.40.16.162>
2015-02-25 08:35:34 -0700

    Detective Wizard

--- wizardDetective/src/byui/cit260/detectiveWizard/view/GameMenuView.java
+++ wizardDetective/src/byui/cit260/detectiveWizard/view/GameMenuView.java
@@ -5,9 +5,73 @@
  */
 package byui.cit260.detectiveWizard.view;
 
+import java.util.Scanner;
+
 /**
 *
 * @author lauryn
 */
 public class GameMenuView {
+    
+    private final String MENU = "\n"
+            + "\n------------------------"
+            + "\nI | Investigate"
+            + "\nV | View Score"
+            + "\nE | Exit";
+           
+void displayMenu(){
+    
+    char selection = ' ';
+    do {
+        
+        System.out.println(MENU);
+        
+        String input= this.getInput();
+        
+        selection = input.charAt(0);
+        
+        this.doAction(selection);
+        
+    }
+    while (selection != 'E');
+}
+
+private String getInput() {
+        //indicates if input has been retrieved
+        boolean valid = false;
+        String input = null;
+        //keyboard input stream
+        Scanner keyboard = new Scanner(System.in);
+        
+        //while a valid value has has not been retrieved
+        while (!valid){
+            //prompt for the player's input
+            System.out.println("Enter menu selection below:");
+            
+            //get the input from the keyboard and trim of the blanks
+            input = keyboard.nextLine();
+            input = input.trim();
+            
+           
+            if (input.length() < 1){
+                System.out.println("\n***Invalid Selection *** Try again ");
+                //and repeat again
+                continue;
+                
+            }
+            //exit the repetition
+            break;   
+        
+        }
+
+        return input;//return the name
+                }
+
+    private void getInput() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void doAction(char selection) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
