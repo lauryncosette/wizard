5aa34c00a3aa78d74b74bda236442e0aeed761d1
Cassandra <Cassandra@VanLydegraf>
2015-03-03 12:57:57 -0700

    created View class
     - promptMessage
     - display()

--- wizardDetective/src/byui/cit260/detectiveWizard/view/View.java
+++ wizardDetective/src/byui/cit260/detectiveWizard/view/View.java
@@ -13,4 +13,28 @@ import byui.cit260.detectiveWizard.ViewInterface;
  */
 public abstract class View implements ViewInterface{
     
+    
+    private String promptMessage;
+    
+    @Override
+    public void display() {
+        
+        String value;
+        //char selection = ' '; is replaced with value
+        do {
+            
+            //displays the MainMenu
+            System.out.println(this.promptMessage);
+            
+            //get user's selection
+            value = this.getInput();
+            //Get the first character of the string
+            //selection = input.charAt(0);
+            
+            //do action based on selection
+            this.doAction(value);
+        }
+        //an selection is not "Exit"
+        while (!value.equals("E"));
+    }
 }
