586225e50139566f630fe12ac243e04d062739af
Cassandra <Cassandra@VanLydegraf>
2015-03-03 13:37:18 -0700

    finished View class

--- wizardDetective/src/byui/cit260/detectiveWizard/view/View.java
+++ wizardDetective/src/byui/cit260/detectiveWizard/view/View.java
@@ -6,6 +6,7 @@
 package byui.cit260.detectiveWizard.view;
 
 import byui.cit260.detectiveWizard.ViewInterface;
+import java.util.Scanner;
 
 /**
  *
@@ -15,6 +16,10 @@ public abstract class View implements ViewInterface{
     
     
     private String promptMessage;
+    
+    public View(String promptMessage){
+        this.promptMessage = promptMessage;
+    }
     
     @Override
     public void display() {
@@ -37,4 +42,46 @@ public abstract class View implements ViewInterface{
         //an selection is not "Exit"
         while (!value.equals("E"));
     }
+    
+    @Override
+    public String getInput() {
+        //indicates if input has been retrieved
+        boolean valid = false;
+        String input = null;
+        //keyboard input stream
+        Scanner keyboard = new Scanner(System.in);
+        
+        //while a valid value has has not been retrieved
+        while (!valid){
+            //prompt for the player's input
+            System.out.println("Enter menu selection below:");
+            
+            //get the input from the keyboard and trim of the blanks
+            input = keyboard.nextLine();
+            input = input.trim();
+            
+            //if the input is invalid(may need help on this) - code is running to this point - when change to <2 code outputs invalid input
+            if (input.length() < 1){
+                System.out.println("*** Invalid input *** input must not be blank");
+                //and repeat again
+                continue;
+                
+            }
+            //exit the repetition
+            break;   
+        
+        }
+        //return the input
+        return input;
+    }
+
+    public String getPromptMessage() {
+        return promptMessage;
+    }
+
+    public void setPromptMessage(String promptMessage) {
+        this.promptMessage = promptMessage;
+    }
+    
+    
 }
