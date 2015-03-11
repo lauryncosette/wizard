e8942b4050d332c45ca9b0cd0c261114278a3b9c
Cassandra <Cassandra@VanLydegraf>
2015-03-11 11:48:40 -0600

    modified map class model layer
    call location array

--- wizardDetective/src/byui/cit260/detectiveWizard/model/Map.java
+++ wizardDetective/src/byui/cit260/detectiveWizard/model/Map.java
@@ -4,7 +4,16 @@ package byui.cit260.detectiveWizard.model;
 import java.io.Serializable;
 
 public class Map implements Serializable{
-    //There are currently no instances to compile in the map
+    
+    private Location[] location;
+    
+    public Location[] getLocation(){
+        return location;
+    }
+    
+    public void setLocation(Location[] location){
+        this.location = location;
+    }
     
     //show character location - array list - characters can be in one location at a time, but have the ability to move
     
