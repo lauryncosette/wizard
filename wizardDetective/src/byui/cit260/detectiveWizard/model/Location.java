481c0be17cfcc49a7e31d3e44e6d7c57f407db2e
Cassandra <Cassandra@VanLydegraf>
2015-03-11 12:00:16 -0600

    modified location class model layer
    turned class into enum
    
    created enum list
    
    deleted unnecessary code

--- wizardDetective/src/byui/cit260/detectiveWizard/model/Location.java
+++ wizardDetective/src/byui/cit260/detectiveWizard/model/Location.java
@@ -1,54 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package byui.cit260.detectiveWizard.model;
 
 import java.io.Serializable;
-import java.util.Objects;
-/**
- *
- * @author Cassandra
- */
-public class Location implements Serializable{
+
+public enum Location implements Serializable{
+    
+    Kitchen(),
+    CrimeScene(),
+    MainLobby(),
+    JanitorialOffice(),
+    JanitorCloset(),
+    WineCellar(),
+    BanquetHall(),
+    LaundryRoom(),
+    Elevator(),
+    GuestRoomEmpty(),
+    Lounge(),
+    ManagerOffice();
     
     //class instances
     private String coordinate;
     
     //alt-fn-insert (Source-Insert Code), constructor, (do not select anything) generate
-    public Location() {
-    }
-    
-    //toString()
-    //alt-fn-insert (Source-Insert Code), toString(), (select all) generate
-    @Override
-    public String toString() {
-        return "Location{" + "coordinate=" + coordinate + '}';
-    }
-    
-    //equals(), hashCode()
-    //alt-fn-insert (Source-Insert Code), equals() and hashCode(), (select all)generate
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 67 * hash + Objects.hashCode(this.coordinate);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Location other = (Location) obj;
-        if (!Objects.equals(this.coordinate, other.coordinate)) {
-            return false;
-        }
-        return true;
+    Location() {
     }
     
     public String getCoordinate(){
