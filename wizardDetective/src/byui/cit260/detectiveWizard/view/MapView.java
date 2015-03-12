dde265b227881075a2c33ed1f270c143f4804880
Mathew <Mathew@Desktop>
2015-03-11 18:50:21 -0700

    Completed basic game map - requirements for game is a minimum of 5x5 map

--- wizardDetective/src/byui/cit260/detectiveWizard/view/MapView.java
+++ wizardDetective/src/byui/cit260/detectiveWizard/view/MapView.java
@@ -6,19 +6,19 @@ public class MapView extends View{
     public MapView(){
         super("\n----------------------------------------------------"
             + "\n|          |         |         |         |         |"
-            + "\n|     D    |    C    |    E    |         |         |"
+            + "\n|     D    |    C    |    E    |    9    |    P    |"
             + "\n|          |         |         |         |         |"
             + "\n|--------------------------------------------------|"
             + "\n|          |         |         |         |         |"
-            + "\n|          |         |         |         |         |"
-            + "\n|          |         |         |         |         |"
-            + "\n|--------------------------------------------------|"
-            + "\n|          |         |         |         |         |"
-            + "\n|          |         |         |         |    R    |"
+            + "\n|     8    |    7    |    6    |    5    |    S    |"
             + "\n|          |         |         |         |         |"
             + "\n|--------------------------------------------------|"
             + "\n|          |         |         |         |         |"
-            + "\n|     W    |         |         |         |    J    |"
+            + "\n|     4    |    3    |    2    |    1    |    R    |"
+            + "\n|          |         |         |         |         |"
+            + "\n|--------------------------------------------------|"
+            + "\n|          |         |         |         |         |"
+            + "\n|     W    |    Z    |    H    |    0    |    J    |"
             + "\n|          |         |         |         |         |"
             + "\n|--------------------------------------------------|"
             + "\n|          |         |         |         |         |"
@@ -29,23 +29,32 @@ public class MapView extends View{
             + "\n----------------------------------------------------"
             + "\n| Map Options                                      |"
             + "\n----------------------------------------------------"
-            + "\nB - Banquet Hall"
-            + "\nC - Crime Scene - Presidential Suite"
-            + "\nD - Presidential Suite - Bathroom"
-            + "\nE - Presidential Suite - Bedroom"
-            //+ "\nG - Guest Room 100s"
-            + "\nI - Janitor's Closet"
-            + "\nJ - Janitorial Office"
-            + "\nK - Kitchen"
-            + "\nL - Lobby"
-            + "\nM - Manager Office"
+            + "\nB - Banquet Hall               0 - Room 100"
+            + "\nC - Crime Scene - Main         1 - Room 101"
+            + "\nD - Crime Scene - Bathroom     2 - Room 102"
+            + "\nE - Crime Scene - Bedroom      3 - Room 103"
+            + "\nH - Main Hall                  4 - Room 104"
+            + "\nI - Janitor's Closet           5 - Room 105"
+            + "\nJ - Janitorial Office          6 - Room 106"
+            + "\nK - Kitchen                    7 - Room 107"
+            + "\nL - Lobby                      8 - Room 108"
+            + "\nM - Manager Office             9 - Room 109"
+            + "\nP - Pool"
             + "\nR - Laundry Room"
-            + "\nW - Wine Cellar");
+            + "\nS - Sauna"
+            + "\nW - Wine Cellar"
+            + "\nZ - Storage");
 }
 
     @Override
     public void doAction(String value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        switch (value.charAt(0)) {
+            case 'E':
+                break;
+            default:
+                System.out.println("\n*** Invalid selection *** Try again");
+        }
     }
     
 }
