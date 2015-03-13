24869222a3aa81344b0c83da3aa2ba8649aa9a4e
Cassandra <Cassandra@VanLydegraf>
2015-03-13 11:51:03 -0600

    inventory array added items

--- wizardDetective/src/byui/cit260/detectiveWizard/control/GameControl.java
+++ wizardDetective/src/byui/cit260/detectiveWizard/control/GameControl.java
@@ -31,7 +31,47 @@ public static void createNewGame(Player player){
     }
 
     public static InventoryItem[] createInventoryList() {
-        System.out.println("*** called createInventoryList() ***");
-        return null;
+        
+        //create inventory array(list)
+        InventoryItem[] inventory =
+                new InventoryItem[5];
+        
+        InventoryItem blood = new InventoryItem();
+        blood.setItemName("blood splatter");
+        blood.setDescription("blood found around victims body");
+        //blood.setItemType("notebook - clue");
+        inventory[0] = blood;
+        
+        InventoryItem hair = new InventoryItem();
+        hair.setItemName("dark hair");
+        hair.setDescription("lock of dark hair found in victims hand");
+        //hair.setItemType("notebook - clue");
+        inventory[1] = hair;
+        
+        InventoryItem pentagram = new InventoryItem();
+        pentagram.setItemName("pentagram");
+        pentagram.setDescription("pentagram inscribed on the floor around the body");
+        //pentagram.setItemType("notebook - clue");
+        inventory[2] = pentagram;
+        
+        InventoryItem key = new InventoryItem();
+        key.setItemName("key");
+        key.setDescription("key to one of the 'employee only' rooms");
+        //key.setUse("opens door to wine cellar")
+        inventory[3] = key;
+        
+        InventoryItem murderWeapon = new InventoryItem();
+        murderWeapon.setItemName("murder weapon");
+        murderWeapon.setDescription("contains traces of victims blood but no fingerprints to help pinpoint the victim");
+        //murderWeapon.setItemType("notebook - clue")
+        inventory[4] = murderWeapon;
+        
+        InventoryItem caseFile = new InventoryItem();
+        caseFile.setItemName("Past case file");
+        caseFile.setDescription("previous case where victim was killed in an identical fashion, happened just down the street, exactly a year to the date");
+        //caseFile.setItemType("notebook - clue")
+        inventory[5] = caseFile;
+        
+        return inventory;
     }
 }
