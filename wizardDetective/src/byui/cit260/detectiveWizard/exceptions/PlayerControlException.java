aead5881505ce44ac2dc8af0111137df4a8e4955
Cassandra <Cassandra@VanLydegraf>
2015-03-21 13:15:04 -0600

    3.21.2015
    created exception functions in exception classes

--- wizardDetective/src/byui/cit260/detectiveWizard/exceptions/PlayerControlException.java
+++ wizardDetective/src/byui/cit260/detectiveWizard/exceptions/PlayerControlException.java
@@ -1,14 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package byui.cit260.detectiveWizard.exceptions;
 
-/**
- *
- * @author Cassandra
- */
-public class PlayerControlException {
-    
+public class PlayerControlException extends Exception {
+
+    public PlayerControlException() {
+    }
+
+    public PlayerControlException(String message) {
+        super(message);
+    }
+
+    public PlayerControlException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PlayerControlException(Throwable cause) {
+        super(cause);
+    }
+
+    public PlayerControlException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
